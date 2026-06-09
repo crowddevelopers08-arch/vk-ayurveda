47eb707054c3ff79da5f697dfab8bc38528e1de2
crowddevelopers08-arch <crowddevelopers08@gmail.com>
2026-06-09 17:13:10 +0530

    vk ayurveda lp added a meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -88,6 +88,30 @@ export default function RootLayout({
           `}
         </Script>
 
+        {/* Facebook Meta Pixel */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;
+            n.push=n;
+            n.loaded=!0;
+            n.version='2.0';
+            n.queue=[];
+            t=b.createElement(e);
+            t.async=!0;
+            t.src=v;
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}
+            (window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '1013044221106865');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
         {/* Microsoft Clarity */}
         <Script id="microsoft-clarity" strategy="afterInteractive">
           {`
@@ -106,6 +130,17 @@ export default function RootLayout({
       </head>
 
       <body className="min-h-full flex flex-col">
+        {/* Facebook Pixel NoScript Fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1013044221106865&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+
         {children}
       </body>
     </html>
